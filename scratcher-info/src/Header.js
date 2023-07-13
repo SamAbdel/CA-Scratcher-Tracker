ad129f2a134df2ebfe423f222c07320fc04d2881
SamAbdel <119086762+SamAbdel@users.noreply.github.com>
2023-07-12 17:39:08 -0700

    Added About Page

--- scratcher-info/src/Header.js
+++ scratcher-info/src/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
 const Header = () => {
-    return (
-      <header className="header-container">
-        <div className="header-title">
-          CA Lottery Tracker
-        </div>
-        <div className="header-links">
-          <a href="/all-scratchers">All Scratchers</a>
-          <a href="/more-info">More Info</a>
-          <a href="https://example.com">CA Lottery Main Site</a>
-        </div>
-      </header>
-    );
+  const handleAboutClick = () => {
+    window.location.href = '/About'; // Change the URL to the About page
   };
+
+  return (
+    <header className="header-container">
+      <div className="header-title">
+        CA Lottery Tracker
+      </div>
+      <div className="header-links">
+        <a href="/all-scratchers">All Scratchers</a>
+        <a href="#" onClick={handleAboutClick}>About</a> {/* Add onClick event handler */}
+        <a href="https://example.com">CA Lottery Main Site</a>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
